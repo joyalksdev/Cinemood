b94da57a042eed64ecd315cb5e1752d579b8f5a6
Joyal <joyalksdev@gmail.com>
2026-01-02 21:46:31 +0530

    Fixed routing and landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import { BrowserRouter, Routes,Route, Navigate } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import RootLayout from './components/layout/RootLayout'
 import Home from './pages/Home'
 import MovieDetails from './pages/MovieDetails'
@@ -10,36 +9,27 @@ import ProtectedRoute from './routes/ProtectedRoute'
 import { useUser } from './context/UserContext'
 import Landing from './pages/Landing'
 
-
-
-
 const App = () => {
-const { user, loading } = useUser();
-if (loading) return null
+  const { user, loading } = useUser()
+  if (loading) return null
+
   return (
-    <BrowserRouter>
-      <Routes>
+    <Routes>
+      <Route path="/" element={user ? <Navigate to="/home" /> : <Navigate to="/landing" />} />
+      <Route path="/get-started" element={<GetStarted />} />
+      <Route path="/landing" element={<Landing />} />
 
-        <Route path="/" element={user ? <Navigate to="/home" /> : <Navigate to="/landing" />} />
-
-
-        <Route path='/get-started' element={<GetStarted />} />
-        <Route path='/landing' element={<Landing />} />
-
-        <Route element={
-          <ProtectedRoute>
-            <RootLayout />
-          </ProtectedRoute>
-        }>
-
-          <Route path="/home" element={<Home />} />
-          <Route path="browse" element={<BrowseMovies />} />
-          <Route path="watchlist" element={<Watchlist />} />
-          <Route path="movie/:id" element={<MovieDetails />} />
-        </Route>
-
-      </Routes>
-    </BrowserRouter>
+      <Route element={
+        <ProtectedRoute>
+          <RootLayout />
+        </ProtectedRoute>
+      }>
+        <Route path="/home" element={<Home />} />
+        <Route path="/browse" element={<BrowseMovies />} />
+        <Route path="/watchlist" element={<Watchlist />} />
+        <Route path="/movie/:id" element={<MovieDetails />} />
+      </Route>
+    </Routes>
   )
 }
 
